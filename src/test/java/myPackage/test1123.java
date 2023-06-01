0de07964b62eac9e315996f556b9bde76f4321da
mohIITpandey <mohitthegreat8@gmail.com>
2023-06-01 15:06:37 +0530

    mohmaya

--- src/test/java/myPackage/test1123.java
+++ src/test/java/myPackage/test1123.java
@@ -1,12 +1,18 @@
 package myPackage;
 
+import org.testng.annotations.Test;
+
 public class test1123 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("mohit");
 
-//notificationNew
+////notificationNew
 	}
 
+@Test
+public void test1() {
+	System.out.println("mohmaya");
+}
 }
